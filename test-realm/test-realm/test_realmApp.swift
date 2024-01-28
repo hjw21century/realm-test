63a510691f1e137baae75e486cc1ddd534c9806d
sonoma <sonoma@byrons-MacBook-Pro.local>
2024-01-28 22:33:37 +0800

    first commit

---
+++ test-realm/test-realm/test_realmApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_realmApp.swift
+//  test-realm
+//
+//  Created by sonoma on 2024/1/28.
+//
+
+import SwiftUI
+
+@main
+struct test_realmApp: SwiftUI.App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
